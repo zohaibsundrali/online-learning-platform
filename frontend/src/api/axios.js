ba73c4e5f8de20c53ef3407a3574dbf3f8982707
Muhammad Zohaib Hassan <159900252+zohaibsundrali@users.noreply.github.com>
2026-06-30 20:57:06 +0500

    Axios Backend Url change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://online-learning-platform-seven-kappa.vercel.app/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
